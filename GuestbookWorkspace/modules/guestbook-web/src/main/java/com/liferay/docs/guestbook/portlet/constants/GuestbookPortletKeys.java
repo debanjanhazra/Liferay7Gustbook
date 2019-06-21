9543f632b1742f9bfbb089142ad03231735a36b5
Debanjan Hazra <Debanjan Hazra@DESKTOP-TTE2SR2>
2019-06-22 01:59:56 +0530

    initia version

---
+++ GuestbookWorkspace/modules/guestbook-web/src/main/java/com/liferay/docs/guestbook/portlet/constants/GuestbookPortletKeys.java
@@ -0,0 +1,10 @@
+package com.liferay.docs.guestbook.portlet.constants;
+
+/**
+ * @author Debanjan Hazra
+ */
+public class GuestbookPortletKeys {
+
+	public static final String Guestbook = "com_liferay_docs_guestbook_portlet_GuestbookPortlet";
+	public static final String GUESTBOOK_ADMIN = "com_liferay_docs_guestbook_portlet_GuestbookAdminPortlet";
+}
