841b73f021c577ec28d1b786372f9c224b3000f6
Debanjan Hazra <Debanjan Hazra@DESKTOP-TTE2SR2>
2020-03-14 23:28:16 +0530

    ok

---
+++ GuestbookWorkspace/modules/serviceWrapper/src/main/java/com/custom/login/CustomLoginServiceActivator.java
@@ -0,0 +1,19 @@
+package com.custom.login;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ * @author Debanjan Hazra
+ */
+public class CustomLoginServiceActivator implements BundleActivator {
+
+	@Override
+	public void start(BundleContext bundleContext) throws Exception {
+	}
+
+	@Override
+	public void stop(BundleContext bundleContext) throws Exception {
+	}
+
+}
