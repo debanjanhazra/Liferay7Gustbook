8d2946b946f86dc216f4bde376bfb610bdb17668
Abhishek <abhishek@creanttechnologies.com>
2020-05-29 21:02:59 +0530

    changed visibility

---
+++ STC/modules/LoginPortlet/src/main/java/com/stc/login/constants/CustomLoginPortletKeys.java
@@ -0,0 +1,11 @@
+package com.stc.login.constants;
+
+/**
+ * @author Debanjan Hazra
+ */
+public class CustomLoginPortletKeys {
+
+	public static final String CUSTOMLOGIN =
+		"com_stc_login_CustomLoginPortlet";
+
+}
---
+++ STC/modules/LoginPortlet/src/main/java/com/stc/login/portlet/CustomLoginPortlet.java
@@ -0,0 +1,30 @@
+package com.stc.login.portlet;
+
+import com.stc.login.constants.CustomLoginPortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Debanjan Hazra
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=STC",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=CustomLogin",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + CustomLoginPortletKeys.CUSTOMLOGIN,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class CustomLoginPortlet extends MVCPortlet {
+}
